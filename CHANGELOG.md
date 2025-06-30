aed0f07f3c984295f328797743b6918a5f3cc6c3
HenriqueResende <hspecian@pucpr.br>
2025-06-30 14:06:27 -0300

    docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## [1.0.0] - 2025-06-30
+
+### Added
+- Auto configurable dynamic Define Symbols by Rules
+- Rule for Package Define Symbols
+- Config file to center Rules
+
+
 ## [0.0.1] - 2025-06-26
 
 ### Added
--- Scripts/Runtime/Interfaces/ISymbolRule.cs
+++ Scripts/Runtime/Interfaces/ISymbolRule.cs
@@ -6,25 +6,4 @@ namespace PUCPR.AutoDefineSymbols
     {
         public ConditionalSymbolData[] GetSymbolAndConditionals(string[] refs);
     }
-
-    public abstract class ASymbolRule
-    {
-        
-        public string[] symbolRefs = { };
-
-        public abstract ConditionalSymbolData[] getSymbolAndConditionals(string[] refs);
-    }
-
-
-    [Serializable]
-    public class SymbolRefs
-    {
-        public ISymbolRule symbolRules;
-        public string[] symbolNames /*= new List<string>()*/;
-
-        public SymbolRefs(ISymbolRule _symbolRules)
-        {
-            symbolRules = _symbolRules;
-        }
-    }
 }
